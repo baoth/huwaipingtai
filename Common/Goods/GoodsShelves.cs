70362793aa94e8d7427128c8491eb04364931858
lbmax0211 <lbmax0211@163.com>
2014-12-05 17:30:01 +0800

    order orderdetail

---
+++ Common/Goods/GoodsShelves.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QSmart.Core.Object;
+namespace DataModel.Goods
+{
+    /// <summary>
+    /// 商品上架表
+    /// </summary>
+    public class GoodsShelves : QSmartEntity
+    {
+        /// <summary>
+        /// 商品编码
+        /// </summary>
+        [PrimaryKey]
+        public int Sku { get; set; }
+        /// <summary>
+        /// 商品描述
+        /// </summary>
+        [StringMaxLength(50, VarCharType.nvarchar)]
+        public string Description { get; set; }
+        /// <summary>
+        /// 商品单价
+        /// </summary>
+        public decimal Price { get; set; }
+    }
+}

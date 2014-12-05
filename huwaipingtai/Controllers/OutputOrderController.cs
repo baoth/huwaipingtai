b85e816f8f43b6a1d605dee411d19b4e57974540
shixb <tzweilai@163.com>
2014-12-05 13:34:16 +0800

    1

---
+++ huwaipingtai/Controllers/OutputOrderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace huwaipingtai.Controllers
+{
+    public class OutputOrderController : Controller
+    {
+        //
+        // GET: /OutputOrder/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        { 
+            return View("Detail");
+        }
+
+    }
+}

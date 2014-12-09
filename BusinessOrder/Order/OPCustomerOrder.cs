d8773f612ebc9a5a43abed9324c84bcdce25c4e1
hanyuxing5588 <707297069@qq.com>
2014-12-09 15:26:34 +0800

    1

---
+++ BusinessOrder/Order/OPCustomerOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IBusinessOrder.Order;
+
+namespace BusinessOrder.Order
+{
+    public class OPCustomerOrder : IOPCustomerOrder
+    {
+        //提交客户订单
+        public bool SubmitOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BusinessOrder/Store/OPStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IBusinessOrder.Store;
+using DataModel.Store;
+namespace BusinessOrder.Store
+{
+    public class OPStore : IOPStore
+    {
+        public List<SKUStore> GetGoodsStore(List<int> listSKU)
+        {
+           return new List<SKUStore>(){
+                new SKUStore(){SKU=10001,StoreCount=100}
+           };
+        }
+    }
+}

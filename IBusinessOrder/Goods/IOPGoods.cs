e489a5664012f75a1a93d25ac7e84f1851895cea
hanyuxing5588 <707297069@qq.com>
2014-12-17 17:09:40 +0800

    123

--- IBusinessOrder/Goods/IOPGoods.cs
+++ IBusinessOrder/Goods/IOPGoods.cs
@@ -9,14 +9,28 @@ namespace IBusinessOrder.Goods
    public interface IOPGoods
     {
        /// <summary>
-       /// 获取当前的模版
+       /// 获取当前商品的模版
        /// </summary>
        /// <returns></returns>
-       string GetGoodsCurTemplate();
-
+       string GetGoodsCurTemplate(string goodsSKU);
+       /// <summary>
+       /// 根据sku获取商品尺码
+       /// </summary>
+       /// <param name="goodsSKU"></param>
+       /// <returns></returns>
        List<GoodsSizeDto> GetGoodsSize(string goodsSKU);
+       /// <summary>
+       /// 根据sku获取商品颜色
+       /// </summary>
+       /// <param name="goodsSKU"></param>
+       /// <returns></returns>
        List<GoodsColorDto> GetGoodsColor(string goodsSKU);
-       List<GoodsDto> GetGoods(string goodsSKU);
+       /// <summary>
+       /// 根据SKU获取商品信息
+       /// </summary>
+       /// <param name="goodsSKU"></param>
+       /// <returns></returns>
+      GoodsDto GetGoods(string goodsSKU);
 
     }
 }

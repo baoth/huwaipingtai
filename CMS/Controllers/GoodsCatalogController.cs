7243bf319a670f61b13c42e8559a5967d0624c7c
shixb <tzweilai@163.com>
2014-12-18 17:29:41 +0800

    1

--- CMS/Controllers/GoodsCatalogController.cs
+++ CMS/Controllers/GoodsCatalogController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using CMS.Models;
 using QSmart.Core.Object;
-using CMS.Common;
+using Toolkit.JsonHelp;
 using Toolkit.CommonModel;
 namespace CMS.Controllers
 {

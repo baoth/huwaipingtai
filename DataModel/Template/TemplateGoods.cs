98707a03ca208bccaeeabba32af517efe8fc6af7
hanyuxing5588 <707297069@qq.com>
2014-12-18 17:15:15 +0800

    123123

---
+++ DataModel/Template/TemplateGoods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QSmart.Core.Object;
+
+namespace DataModel.Template
+{
+    public class TemplateGoods : QSmartEntity
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+        public int GoodsId { get; set; }
+        public int TempalteId { get; set; }
+    }
+}

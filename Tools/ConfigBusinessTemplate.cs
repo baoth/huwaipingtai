d90c3d551ff6924f70374fca8267763d939365a8
hanyuxing5588 <707297069@qq.com>
2014-12-18 16:42:24 +0800

    1

--- Tools/ConfigBusinessTemplate.cs
+++ Tools/ConfigBusinessTemplate.cs
@@ -22,6 +22,7 @@ namespace BusinessTemplate
             builder.RegisterType<BusinessOrder.Cart.OPCart>().As<IBusinessOrder.Cart.IOPCart>();
             builder.RegisterType<BusinessOrder.Order.OPCustomerOrder>().As<IBusinessOrder.Order.IOPCustomerOrder>();
             builder.RegisterType<BusinessOrder.Store.OPStore>().As<IOPStore>();
+            builder.RegisterType<BusinessOrder.CMS.OPGoods>().As<IOPGoods>();
             builder.RegisterType<BusinessOrder.CMS.Publish>().As<IPublish>();
         }
         public static IBusinessOrder.User.ILogon GetILogon()

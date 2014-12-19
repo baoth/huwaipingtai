6c0f54eebefdd7c521047aa98a882e5dd883fd8e
hanyuxing5588 <707297069@qq.com>
2014-12-19 15:22:12 +0800

    1

--- huwaipingtai/Controllers/TestController.cs
+++ huwaipingtai/Controllers/TestController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QSmart.Core.DataBase;
+using DataModel;
 
 namespace huwaipingtai.Controllers
 {
@@ -13,6 +15,7 @@ namespace huwaipingtai.Controllers
         {
             return View();
         }
+
         public ActionResult CreateTable() 
         {
             QSmartDatabaseClient db = DataBaseProvider.Create("db");

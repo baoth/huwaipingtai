f7d46cab1c05d92c700a2e9b70dbd51f3b6fe430
shixb <tzweilai@163.com>
2014-12-19 15:13:38 +0800

    1

---
+++ huwaipingtai/Controllers/GoodsSizeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace huwaipingtai.Controllers
+{
+    public class GoodsSizeController : Controller
+    {
+        //
+        // GET: /GoodsSize/
+
+        public ActionResult Index()
+        {
+            return View("index");
+        }
+        public ActionResult editSize()
+        {
+            return View("editSize");
+        }
+
+    }
+}

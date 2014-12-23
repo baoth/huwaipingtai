41975450c9eee5b7c80ec65df80f1a61f112beb4
hanyuxing5588 <707297069@qq.com>
2014-12-23 11:41:16 +0800

    12

---
+++ BusinessOrder/StorageIn/StorageIn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IBusinessOrder.StorageIn;
+using DataModel;
+
+namespace BusinessOrder.StorageIn
+{
+    public class StorageIn : IStorageIn
+    {
+
+        public IList<DataModel.YanSe> GetColorByGoodsId(string gid)
+        {
+            return new List<DataModel.YanSe>()
+            {
+              new DataModel.YanSe(){Name="深灰色",Id=1},
+              new DataModel.YanSe(){Name="藏青色",Id=2},
+              new DataModel.YanSe(){Name="天蓝色",Id=3},
+              new DataModel.YanSe(){Name="宝石绿",Id=4},
+              new DataModel.YanSe(){Name="葡萄紫",Id=5}
+            };
+        }
+
+        public IList<ChiMa> GetSizeByGoodsId(string gid)
+        {
+            return new List<ChiMa>()
+            {
+              new ChiMa(){Name="M",Id=1},
+              new ChiMa(){Name="L",Id=2},
+              new ChiMa(){Name="XL",Id=3},
+              new ChiMa(){Name="XXL",Id=4},
+              new ChiMa(){Name="4XL",Id=5}
+            };
+        }
+    }
+}
---
+++ IBusinessOrder/StorageIn/StorageIn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataModel;
+
+namespace IBusinessOrder.StorageIn
+{
+   public interface IStorageIn
+    {
+       /// <summary>
+       /// 获得商品的尺码
+       /// </summary>
+       /// <returns></returns>
+       IList<ChiMa> GetSizeByGoodsId(string gid);
+
+       /// <summary>
+       /// 获得商品的颜色
+       /// </summary>
+       /// <returns></returns>
+       IList<DataModel.YanSe> GetColorByGoodsId(string gid);
+
+    }
+}

367eb5e4a95e185df2264f50e9a3e5e98f7b66ae
lbmax0211 <lbmax0211@163.com>
2014-12-24 17:42:55 +0800

    111

--- FZWeb/Controllers/TestController.cs
+++ FZWeb/Controllers/TestController.cs
@@ -36,7 +36,7 @@ namespace FZ.Controllers
 
             db.CreateTable<CaoZuoYuan>(QSmartTableType.InnoDB);
             db.CreateTable<MenDian>(QSmartTableType.InnoDB);
-            db.CreateTable<MenDian_CanZuoYuan_GuanLian>(QSmartTableType.InnoDB);
+            //db.CreateTable<MenDian_CanZuoYuan_GuanLian>(QSmartTableType.InnoDB);
 
             db.CreateTable<CangKu>(QSmartTableType.InnoDB);
             db.CreateTable<HuoWei>(QSmartTableType.InnoDB);
@@ -68,14 +68,14 @@ namespace FZ.Controllers
             //List<string> level2name14 = new List<string> { "摄影摄像", "数码配件", "时尚影音", "电子教育", "智能设备"};
             //List<string> level2name15 = new List<string> { "电脑整机", "电脑配件", "外设产品", "网络产品", "办公设备", "文具/耗材" };
             //List<string> level2name16 = new List<string> { "面部护肤", "身体护肤", "口腔护理", "女性护理", "香水彩妆", "洗发护发" };
-
-            cdata(new List<string> { "运动户外" }, 10, "", 1);
+            QSmartDatabaseClient db = DataBaseProvider.Create("db");
+            cdata(db,new List<string> { "运动户外" }, 10, "", 1);
 
 
             List<string> level2 = new List<string> { "户外装备", "健身训练", "纤体瑜伽", 
                                                            "体育用品", "运动鞋包", "运动服饰",
                                                            "骑行运动", "户外鞋服", "垂钓用品","游泳用品"};
-            cdata(level2, 10, "10", 2);
+            cdata(db,level2, 10, "10", 2);
             Dictionary<int, List<string>> level3 = new Dictionary<int, List<string>>()
             {
                 {1010,new List<string>{"帐篷/垫子","旅游用品","便携桌椅床","野餐烧烤","军迷用品","救援装备","滑雪装备","睡袋/吊床",
@@ -88,16 +88,33 @@ namespace FZ.Controllers
                 {1014,new List<string>{"跑步鞋","拖鞋","运动包","休闲鞋","篮球鞋","板鞋","帆布鞋","足球鞋",
                                        "乒羽网鞋","专项运动鞋","训练鞋"}}, 
                 {1015,new List<string>{"羽绒服","运动背心","毛衫/线衫","运动配饰","棉服","运动裤","夹克/风衣","卫衣/套头衫",
-                                       "T恤","套装","乒羽网服","训练服"}}
-            };                    
+                                       "T恤","套装","乒羽网服","训练服"}},
+                {1016,new List<string>{"折叠车","山地车/公路车","电动车","其他整车","骑行服","骑行装备"}},
+                {1017,new List<string>{"软壳衣裤","T恤","户外风衣","功能内衣","军迷服饰","登山鞋","雪地靴","徒步鞋",
+                                       "越野跑鞋","休闲鞋","工装鞋","溯溪鞋","沙滩/凉拖","户外袜","冲锋衣裤","速干衣裤","滑雪服","羽绒服/棉服","休闲衣裤","抓绒衣裤"}},
+                {1018,new List<string>{"鱼竿鱼线","其它","浮漂鱼饵","钓鱼桌椅","钓鱼配件","钓箱鱼包"}},
+                {1019,new List<string>{"泳镜","比基尼","其它","泳帽","游泳包防水包","女士泳衣","男士泳衣"}}
+            };
+            foreach (int key in level3.Keys)
+            {
+                cdata(db,level3[key], 10, key.ToString(), 3);
+            }
+            try
+            {
+                db.SaveChange();
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+            return Content("ok");
         }
 
 
-        private string cdata(List<string> names, int beginbianma, string pbinama,int level)
+        private void cdata(QSmartDatabaseClient db, List<string> names, int beginbianma, string pbinama,int level)
         {
-            try
-            {
-                QSmartDatabaseClient db = DataBaseProvider.Create("db");
+            
+                
                 foreach (string name in names)
                 {
                     db.InsertEntity(new ShangPinFenLei
@@ -109,14 +126,34 @@ namespace FZ.Controllers
                     }.CreateQSmartObject());
                     beginbianma++;
                 }
-                db.SaveChange();
-            }
-            catch (Exception ex)
-            {
-                return ex.Message;
-            }
-            return "ok";
+                
+            
         }
 
+
+        public ActionResult yansechima()
+        {
+            QSmartDatabaseClient db = DataBaseProvider.Create("db");
+            db.InsertEntity(new YanSe { Name = "红色" }.CreateQSmartObject());
+            db.InsertEntity(new YanSe { Name = "黑色" }.CreateQSmartObject());
+            db.InsertEntity(new YanSe { Name = "藏蓝色" }.CreateQSmartObject());
+            db.InsertEntity(new YanSe { Name = "白色" }.CreateQSmartObject());
+            db.InsertEntity(new YanSe { Name = "墨绿色" }.CreateQSmartObject());
+            db.InsertEntity(new YanSe { Name = "天青色" }.CreateQSmartObject());
+            db.InsertEntity(new YanSe { Name = "黄色" }.CreateQSmartObject());
+            db.InsertEntity(new YanSe { Name = "绿色" }.CreateQSmartObject());
+            db.InsertEntity(new YanSe { Name = "草绿色" }.CreateQSmartObject());
+            db.InsertEntity(new YanSe { Name = "紫色" }.CreateQSmartObject());
+            db.InsertEntity(new YanSe { Name = "宝石蓝色" }.CreateQSmartObject());
+            db.InsertEntity(new YanSe { Name = "磨砂岩色" }.CreateQSmartObject());
+
+            db.InsertEntity(new ChiMa { Name = "S" }.CreateQSmartObject());
+            db.InsertEntity(new ChiMa { Name = "M" }.CreateQSmartObject());
+            db.InsertEntity(new ChiMa { Name = "L" }.CreateQSmartObject());
+            db.InsertEntity(new ChiMa { Name = "XL" }.CreateQSmartObject());
+            db.InsertEntity(new ChiMa { Name = "XXL" }.CreateQSmartObject());
+            db.SaveChange();
+            return Content("ok");
+        }
     }
 }

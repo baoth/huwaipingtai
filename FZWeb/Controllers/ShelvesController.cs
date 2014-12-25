94ea797b31f612c255e093916785b9d79bd0bfd6
shixb <tzweilai@163.com>
2014-12-25 16:30:42 +0800

    1

--- FZWeb/Controllers/ShelvesController.cs
+++ FZWeb/Controllers/ShelvesController.cs
@@ -3,21 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Toolkit.JsonHelp;
 
 namespace FZ.Controllers
 {
-    public class ShelvesController : Controller
+    public class ShelvesController : BasicController
     {
-        //
-        // GET: /Brand/
-
+        IBusinessOrder.Shelves.IOPShelves iopshelves;
+        public ShelvesController(IBusinessOrder.Shelves.IOPShelves iopshelves)
+        {
+            this.iopshelves = iopshelves;
+        }
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Brand()
+        public ActionResult BrandList()
         {
-            return View("Brand");
+            return View("BrandList");
+        }
+        public ActionResult ProductList()
+        {
+            var pingpaiid=Request["pinpaiid"];
+            if (!string.IsNullOrEmpty(pingpaiid))
+            {
+                ViewData["pinpaiid"] = pingpaiid;
+            }
+            return View("ProductList");
+        }
+        
+        public ActionResult GetBrandList()
+        {
+            try
+            {
+                var list = iopshelves.GetBrandList();
+                var jsonStr = JsonHelp.objectToJson(list);
+                return Content(jsonStr);
+            }
+            catch (Exception ex)
+            {
+                return Content("");
+            }
+        }
+
+        public ActionResult GetProductList()
+        {
+            try
+            {
+                int id;
+                var pingpaiid=Request["pingpaiid"];
+                int.TryParse(pingpaiid,out id);
+                var list = iopshelves.GetProductList(id);
+                var jsonStr = JsonHelp.objectToJson(list);
+                return Content(jsonStr);
+            }
+            catch (Exception ex)
+            {
+                return Content("");
+            }
         }
 
     }

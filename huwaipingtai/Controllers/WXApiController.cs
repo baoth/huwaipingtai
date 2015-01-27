a8da3c6b545d77103d783c40095f7d8f09ad8559
lbmax0211 <lbmax0211@163.com>
2015-01-27 17:03:33 +0800

    weixinpay

--- huwaipingtai/Controllers/WXApiController.cs
+++ huwaipingtai/Controllers/WXApiController.cs
@@ -57,8 +57,25 @@ namespace huwaipingtai.Controllers
                             Logger.Write("关注");
                             return Content(context);
                         case EventType.click:
-                            Logger.Write("点击首页");
-                            return Content(context);
+                            if (baseinfo.EventKey == "V_HOME_INDEX")
+                            {
+                                return Content(context);
+                            }
+                            else
+                            {
+                                ReplyWeixinNewsMessage rnm = new ReplyWeixinNewsMessage();
+                                rnm.FromUserName = baseinfo.ToUserName;
+                                rnm.ToUserName = baseinfo.FromUserName;
+                                rnm.CreateTime = WeixinCoreExtension.GetTimeStamp(DateTime.Now);
+                                ArticleItem item = new ArticleItem();
+                                item.Title = "支付测试";
+                                item.Description = "支付测试";
+                                item.PicUrl = "http://test.nkwang.cn/Product/Images/n1/test/9b839728-d085-468e-acb8-88eb9eb008b8.jpg";
+                                item.Url = "http://test.nkwang.cn/WeixinPay/Index?order_no=5&good_body=bai se 上衣&fee=1&showwxpaytitle=1";
+                                rnm.Articles.Add(item);
+                                return Content(rnm.GetReplyMessage());
+                            }
+                            
                             // Logger.Write("关注");
                             //var context = WeixinAdaptor.GetWxMessage(baseinfo.ToUserName,baseinfo.FromUserName,"");
                             //return Content(context);
@@ -91,7 +108,7 @@ namespace huwaipingtai.Controllers
         /// <returns></returns>
         public ActionResult CreateMenu()
         {
-            var jsonMenu = "{\"button\":[{\"name\":\"首页\", \"type\":\"click\", \"key\":\"V_HOME_INDEX\"}, {"+
+            var jsonMenu = "{\"button\":[{\"name\":\"首页\", \"type\":\"click\", \"key\":\"V_HOME_INDEX\"}, {\"name\":\"支付\", \"type\":\"click\", \"key\":\"V_JSPAY_INDEX\"},{" +
              
            " \"name\": \"发送位置\", "+
          "   \"type\": \"location_select\", "+
@@ -105,32 +122,6 @@ namespace huwaipingtai.Controllers
             return Content("Ok");
         }
 
-        /// <summary>
-        /// 调用微信统一支付
-        /// </summary>
-        /// <returns></returns>
-        public JsonResult JsCommonPay()
-        {
-            //1.获取openid
-            string openid = Request.Cookies["sid"].Value;
-            if (string.IsNullOrEmpty(openid)) return null;
-            //2.生成统一支付xml数据
-            string datastring = (new WeixinJsCommonPay { total_fee = 1, out_trade_no = "1", openid = "aaa", body = "test cloath" }).GetRequestData();
-            WeixinCore wc = WeixinAdaptor.CreateWeixinCore();
-            string resultjson = wc.PostRequest(WeixinJsCommonPay.post_url, datastring);
-            Response.Write(resultjson);
-            return null;
-        }
-
-
-        /// <summary>
-        /// 微信统一支付通知地址
-        /// </summary>
-        /// <returns></returns>
-        public JsonResult CommonPayNotify()
-        {
-            return null;
-        }
 
         ///// <summary>
         ///// 删除菜单
@@ -145,91 +136,5 @@ namespace huwaipingtai.Controllers
         //}
     }
 
-    /// <summary>
-    /// 微信统一支付模型
-    /// </summary>
-    class WeixinJsCommonPay
-    {
-        /// <summary>
-        /// 微信统一支付请求地址
-        /// </summary>
-        public  const string post_url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
 
-        /// <summary>
-        /// 用户标识
-        /// </summary>
-        public string openid { get; set; }
-        /// <summary>
-        /// 商品描述
-        /// </summary>
-        public string body { get; set; }
-        /// <summary>
-        /// 商户订单号
-        /// </summary>
-        public string out_trade_no { get; set; }
-        /// <summary>
-        /// 总金额
-        /// </summary>
-        public int total_fee { get; set; }
-        /// <summary>
-        /// 通知地址
-        /// </summary>
-        protected string notify_url { get { return "http://test.nkwang.cn/WXApi/CommonPayNotify"; } }
-        /// <summary>
-        /// 交易类型
-        /// </summary>
-        protected string trade_type { get { return "JSAPI"; } }
-        /// <summary>
-        /// 公众账号ID
-        /// </summary>
-        protected string appid { get{return WeixinAdaptor.AppId;}}
-        /// <summary>
-        /// 商户号
-        /// </summary>
-        protected string mch_id { get { return WeixinAdaptor.MchId; } }
-        /// <summary>
-        /// 终端IP
-        /// </summary>
-        protected string spbill_create_ip { get { return "114.215.131.65"; } }
-        /// <summary>
-        /// 随机字符串
-        /// </summary>
-        protected string nonce_str { get { return Guid.NewGuid().ToString().Replace("-", ""); } }
-        /// <summary>
-        /// 签名
-        /// </summary>
-        protected string sign { get; set; }
-
-        public string GetRequestData()
-        {
-            this.sign = this.createsign();
-            StringBuilder strb = new StringBuilder();
-            strb.AppendLine("<xml>");
-            strb.AppendLine(string.Format("<openid><![{0}]]></openid>", this.openid));
-            strb.AppendLine(string.Format("<body><![CDATA[{0}]]></body>",this.body));
-            strb.AppendLine(string.Format("<out_trade_no><![CDATA[{0}]]></out_trade_no>",this.out_trade_no));
-            strb.AppendLine(string.Format("<total_fee>{0}</total_fee>",this.total_fee));
-            strb.AppendLine(string.Format("<notify_url><![CDATA[{0}]]></notify_url>",this.notify_url));
-            strb.AppendLine(string.Format("<trade_type><![CDATA[{0}]]></trade_type>",this.trade_type));
-            strb.AppendLine(string.Format("<appid><![CDATA[{0}]]></appid>",this.appid));
-            strb.AppendLine(string.Format("<mch_id>{0}</mch_id>",this.mch_id));
-            strb.AppendLine(string.Format("<spbill_create_ip><![CDATA[{0}]]></spbill_create_ip>",this.spbill_create_ip));
-            strb.AppendLine(string.Format("<nonce_str><![CDATA[{0}]]></nonce_str>",this.nonce_str));
-            strb.AppendLine(string.Format("<sign><![CDATA[{0}]]></sign>",this.sign));
-            strb.AppendLine("</xml>");
-            return strb.ToString();
-        }
-
-        protected string createsign()
-        {
-            string StringA = string.Format("appid={0}&body={1}&mch_id={2}&nonce_str={3}&notify_url={4}&" +
-                                           "openid={5}&out_trade_no={6}&" +
-                                           "spbill_create_ip={7}&trade_type={8}&total_fee={9}",
-                                           this.appid,this.body,this.mch_id,this.nonce_str,this.notify_url,
-                                           this.openid,this.out_trade_no,this.spbill_create_ip,this.trade_type,this.total_fee);
-            string stringSignTemp = StringA + "&key=" + WeixinAdaptor.API密钥;
-
-            return System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(stringSignTemp, "MD5").ToUpper();
-        }
-    }
 }

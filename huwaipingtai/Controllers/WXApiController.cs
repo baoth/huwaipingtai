87b7b8504320fdf1775d484681097632dee70457
lbmax0211 <lbmax0211@163.com>
2015-01-28 17:45:35 +0800

    1111

--- huwaipingtai/Controllers/WXApiController.cs
+++ huwaipingtai/Controllers/WXApiController.cs
@@ -105,7 +105,7 @@ namespace huwaipingtai.Controllers
         /// <returns></returns>
         public ActionResult CreateMenu()
         {
-            var jsonMenu = "{\"button\":[{\"name\":\"首页\", \"type\":\"CLICK\", \"key\":\"home\"}, {\"name\":\"支付\", \"type\":\"CLICK\", \"key\":\"jspay\"},{" +
+            var jsonMenu = "{\"button\":[{\"name\":\"首页\", \"type\":\"click\", \"key\":\"home\"}, {\"name\":\"支付测试\", \"type\":\"click\", \"key\":\"jspay\"},{" +
              
            " \"name\": \"发送位置\", "+
          "   \"type\": \"location_select\", "+
